5813027c45626c1222dd23ca83ee4cc7b92478c0
ArtB <artembykovv@gmail.com>
2021-07-13 16:29:51 +0200

    finish parallel coordinates

--- src/app/Components/Visualizations/ParallelCoordinates.tsx
+++ src/app/Components/Visualizations/ParallelCoordinates.tsx
@@ -1,14 +1,13 @@
-import React, { useMemo, useRef } from 'react'
-import { scaleLinear, scaleOrdinal } from 'd3-scale'
+import React, { useMemo } from 'react'
 import { max, merge } from 'd3-array'
 import { useSelector } from 'react-redux'
 import { Group } from '../../../store/data/data.interfaces'
-import { getAllData } from '../../../store/data/data.selectors'
 import { path } from 'd3-path'
+import { getColorByKey } from '../../../style/theme'
+import { Group } from '@material-ui/icons'
+import { scaleLinear, scaleOrdinal } from 'd3-scale'
 
-
-
-interface  ParallelCoordinatesProps {
+interface ParallelCoordinatesProps {
     groups: Group[]
     w: number
     h: number
@@ -16,116 +15,80 @@ interface  ParallelCoordinatesProps {
 }
 
 const ParallelCoordinates: React.FC<ParallelCoordinatesProps> = (props) => {
-    const labels = props.groups.map(group => group.label)
-    const spacingLeft = 70
-    const spacingBottom = 70
-    const chartWidth = (props.w - (2 * props.pad) - spacingLeft)
-    const chartHeight = (props.h - (2 * props.pad) - spacingBottom)
-    const maxValue = max(merge(props.groups.map(group => [group.Haushaltsbruttoeinkommen, group.Haushaltsnettoeinkommen, group['Ausgabefaehige Einkommen und Einnahmen'], group['Private Konsumausgaben'], group['Andere Ausgaben']])))
-    const axes = ['Haushaltsbruttoeinkommen', 'Haushaltsnettoeinkommen', 'Ausgabefaehige Einkommen und Einnahmen', 'Private Konsumausgaben', 'Andere Ausgaben']
+  const spacingLeft = 70
+  const spacingBottom = 70
+  const chartWidth = (props.w - (2 * props.pad) - spacingLeft)
+  const chartHeight = (props.h - (2 * props.pad) - spacingBottom)
+  const maxValue = max(merge(props.groups.map(group => [group.Haushaltsbruttoeinkommen, group.Haushaltsnettoeinkommen, group['Ausgabefaehige Einkommen und Einnahmen'], group['Private Konsumausgaben'], group['Andere Ausgaben']])))
+  const axes = ['Haushaltsbruttoeinkommen', 'Haushaltsnettoeinkommen', 'Ausgabefaehige Einkommen und Einnahmen', 'Private Konsumausgaben', 'Andere Ausgaben']
 
-    const finance = useSelector(getAllData)
+  const y = useMemo(() => {
+    return scaleLinear().domain([0, maxValue]).range([chartHeight, 0])
+  }, [props.groups])
 
-    const y = useMemo(() => {
-        return scaleLinear().domain([0, maxValue]).range([chartHeight, 0])
-      }, [props.groups])
+  const yTicks = useMemo(() => {
+    return y.ticks()
+  }, [props.groups])
 
+  const color = useMemo(() => {
+    return scaleOrdinal().domain(axes).range(['#1487C2', '#28BCCA', '#C9D93B', '#FEA82A', '#D08AC0'])
+  }, [])
 
+  const xOffSet = chartWidth / axes.length
+  const yOffSet = chartHeight / yTicks.length
 
-    const yTicks = useMemo(() => {
-        return y.ticks()
-      }, [props.groups])
+  return (
 
-
-    const xOffSet = chartWidth / axes.length
-    const yOffSet = chartHeight / yTicks.length
-    
-
-    return (
-
-        <svg width={props.w} height={props.h} style={{ padding: props.pad }}>
-            {/** Y Axis **/}
-            <g>
-                {
-                    axes.map((axe, index) => {
-                        
-                        return (
-                            <g key={index}>
-                                <path d={'M ' + (spacingLeft + ((index) * xOffSet)) + ' 0 L ' + (spacingLeft + ((index) * xOffSet)) + ' ' + chartHeight} stroke='black'></path>
-                            </g>
-                        )
-                    })
-                }
-            </g>
+    <svg width={props.w} height={props.h} style={{ padding: props.pad }}>
+      {/** Y Axis **/}
+      <g>
+        {
+          axes.map((axe, index) => {
+            return (
+              <g key={index}>
+                <path d={'M ' + ((index + 1) * xOffSet) + ' 0 L ' + ((index + 1) * xOffSet) + ' ' + chartHeight} stroke='black' />
+              </g>
+            )
+          })
+        }
+      </g>
         
-            {/** Labels **/}
-            <g>
-                {
+      {/** Labels **/}
+      <g>
+        {
                     
-                    axes.map((axes, index) => {
-                        return (
-                            <g key={index} transform={'translate(' + (spacingLeft + index * xOffSet) + ',' + (chartHeight + 20) + ')rotate(25)'} style={{ fontSize: 12 }}>
-                                <text>{axes}</text>
-                            </g>
-                        )
-                    })
+          axes.map((axe, index) => {
+            return (
+              <g key={index} transform={'translate(' + (spacingLeft + index * xOffSet) + ',' + (chartHeight + 20) + ')rotate(15)'} style={{ fontSize: 9 }}>
+                <text>{axe}</text>
+              </g>
+            )
+          })
+        }
+      </g>
+      {/** Values**/}
+      <g>
+        {
+          props.groups.map((group, index) => {
+            const line = (context) => {
+              axes.forEach((axe, index) => {
+                if (index === 0) {
+                  context.moveTo((index + 1) * xOffSet, y(group[axe]))
+                } else {
+                  context.lineTo((index + 1) * xOffSet, y(group[axe]))
                 }
-            </g>
-            {/** Values**/}
-            <g>
-                {
-                    props.groups.map((group, index) => {
+              })
+              return context
+            }
 
-                        console.log(group)
-
-                        const box = (context) => {
-                            axes.forEach( (axe, index) => {
-                                if (index === 0){
-                                    console.log(y(group[axe]))
-                                    context.moveTo((index + 1)* xOffSet, y(group[axe]))
-                                } else {
-                                    context.lineTo((index + 1) * xOffSet, y(group[axe]))
-                                }
-                            })
-
-                            return context
-                        }
-
-
-                        return <path d={box(path())} fill='none' stroke='lightgrey'></path>
-                    })
-            
+            return <path key={index} d={line(path())} fill='none' stroke={color(index)} strokeWidth={1} />
+          })
                 
-                /**finance.map((type, index) => {
-                    console.log("finance[1] = " + finance[index])
-                    return (
-                        <g key={index}>
-                            {
-                                type.map((values, index) => {
-                                    console.log("values[0] = " + values[0])
+        }
+      </g>
+    </svg>
 
-                                    const box = (context) => {
-                                        context.moveTo(0,0)
-                                        context.lineTo(props.w, 30)
-                                        context.lineTo(props.w, props.h)
-                                        context.lineTo(10, props.h)
-                                        context.lineTo(10, 30)
+  )
+}
 
-                                        return context
-                                    }
-        
-                                    return <path d={box(path())} fill='none' stroke='lightgrey' />
-                                })
-                                
-                            }
-                        </g>
-                    )
-                        })**/
-                }
-            </g>
-        </svg>
-
-  );
-};
-
-export default ParallelCoordinates;
+export default ParallelCoordinates
